41a0a13a42b96e307d949a5a26bca6bd5cb6aea9
Kuzmarg <vovakuz45616@gmail.com>
2021-11-02 17:49:15 +0200

    Update target_ua.py

---
+++ target_ua.py
@@ -0,0 +1,17 @@
+"""
+Why are we still here? Just to suffer?
+"""
+import random
+def generate_grid():
+    """
+    -> list
+    Generates letters list
+    """
+    start_list='абвгґдеєжзиіїйклмнопрстуфхцчшщьюя'
+    res=[]
+    i=0
+    while i<5::
+        random_letter=random.choice(start_list)
+        if random_word not in res:
+            res.append(random_letter)
+            i+=1
